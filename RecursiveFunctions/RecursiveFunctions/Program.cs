c633cc3244e79cc93ff323d0b4552733232b9c88
ibrahim köprücü <43587170+ibrahimkoprucu@users.noreply.github.com>
2019-04-04 01:33:32 +0300

    Some Files Added

---
+++ RecursiveFunctions/RecursiveFunctions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RecursiveFunctions
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            IsimYazdır(5);
+        }
+
+        private static void IsimYazdır(int sayac)
+        {
+            if (sayac == 0)
+            {
+                return;
+            }
+            else
+            {
+                IsimYazdır(sayac - 1);
+            }
+
+            Console.Write("ibo ");
+        }
+    }
+}
